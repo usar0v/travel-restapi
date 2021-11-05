6c28fb0319ee7c5ebf7365f60b5cbc00e266b15a
Usar0v <usar0v@MacBook-Pro-Usar0v.local>
2021-11-05 14:49:19 +0600

    procfile-fix

--- travel/settings.py
+++ travel/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+django_heroku.settings(locals() )
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
