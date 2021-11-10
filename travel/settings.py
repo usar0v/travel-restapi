d97f0165bb22f81b5b9da1d0d8dc80207b198258
Usar0v <usar0v@MacBook-Pro-Usar0v.local>
2021-11-10 13:17:55 +0600

    fix

--- travel/settings.py
+++ travel/settings.py
@@ -143,7 +143,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    '/static/'
+    'static/'
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
